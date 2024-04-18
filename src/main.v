a681c98a109f577310a568bf79e5431e13ba9c24
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-04-18 19:48:34 -0300

    minor changes

--- src/main.v
+++ src/main.v
@@ -1,18 +1,19 @@
 module main(
-input low, 
-input mid, 
-input Us, 
-input Ua,
-input T,
-input high, 
-output watter_supply, 
-output error, 
-output alarme, 
-output asp,
-output got
+	input low, 
+	input mid, 
+	input Us, 
+	input Ua,
+	input T,
+	input high, 
+	output watter_supply, 
+	output error, 
+	output alarme, 
+	output asp,
+	output got,
 );
 
 	wire rega;
+	
 	check_error creitinho_dugrau(low, mid, high, error);
 	open_watter_supply open(error, high, watter_supply);
 	rega rega_tete(error, Us, low,rega);
