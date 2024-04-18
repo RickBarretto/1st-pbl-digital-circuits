d81e00a0f9fc95b556f443fe768cda0728b9629b
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-04-18 19:48:30 -0300

    minor changes

--- src/open_watter_supply.v
+++ src/open_watter_supply.v
@@ -1,3 +1,9 @@
-module open_watter_supply(input error, input high, output valvule);
+module open_watter_supply(
+	input error, 
+	input high, 
+	
+	output valvule);
+
 	nor low_and_right(valvule, error, high);
+
 endmodule
