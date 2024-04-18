26bd5caf878ec85cbd878e648b853776d70a24bb
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-04-18 20:45:42 -0300

    create a clean script based on gitignore file

---
+++ tools/clean.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+project = "irrigation-system"
+git_ignore = Path(".gitignore").read_text()
+source = Path("./src/")
+
+directories: list[Path] = []
+
+print(f"Cleaning {project}'s ./{source}...")
+
+for pattern in git_ignore.splitlines():
+    for file in source.glob(pattern):
+        if file.is_file:
+            print(file)
+            file.unlink()
+        elif file.is_dir:
+            directories.append(file)
+
+for directory in directories:
+    print(directory)
+    directory.rmdir()
