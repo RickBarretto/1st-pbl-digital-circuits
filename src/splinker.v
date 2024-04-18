5ddc8d542f0b989fa92d86cd2ab7b60512c2c48f
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-04-18 19:49:59 -0300

    rename asp.v and minor changes

---
+++ src/splinker.v
@@ -0,0 +1,18 @@
+module splinker(
+    input solo, 
+    input ar, 
+    input T, 
+    input mid, 
+    output asp,
+);
+
+    wire s1;
+
+    not notS(s1, solo);
+    not notA(s2, ar);
+    not notT(s3, T);
+    and and1(s4, s1, s2);
+    and and2(s5, s1, s3, mid);
+    or or1(asp, s4, s5);
+
+endmodule 
