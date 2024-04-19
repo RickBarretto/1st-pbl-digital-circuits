99a5780ddc628db47937bc0d30098586016fb33c
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-04-18 21:16:47 -0300

    [sensor_checker]: give semantic names to variables

--- src/watter_supply/sensor_checker.v
+++ src/watter_supply/sensor_checker.v
@@ -1,12 +1,18 @@
-module sensor_checker(input low, input mid, input high, output error);
+module sensor_checker(
+	input low_watter_level, 
+	input mid_watter_level, 
+	input high_watter_level, 
+
+	output error
+);
 
 	wire s1, s2, n1, n2;
 	
-	not not_0(n1, mid);
-	not not_1(n2, high);
+	not not_0(n1, mid_watter_level);
+	not not_1(n2, high_watter_level);
 	and and_1(s1, n1, n2);
 	
-	and and_2(s2, low, mid);
+	and and_2(s2, low_watter_level, mid_watter_level);
 	
 	nor or_0(error, s1, s2);
 
