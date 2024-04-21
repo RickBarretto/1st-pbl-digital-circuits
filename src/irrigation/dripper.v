70e0de4040b02108a7c7bbee3fa675b9493ad657
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-04-20 21:52:26 -0300

    [dripper] give wire a better naming based on the documentation

--- src/irrigation/dripper.v
+++ src/irrigation/dripper.v
@@ -13,13 +13,13 @@ module dripper(
     input mid_water_level
 );
 
-    wire low_water_level, s2, environment_conditions;
+    wire low_water_level, case2, environment_conditions;
 
     and and1(environment_conditions, air_humidity, low_temperature);
 
     not not1(low_water_level, mid_water_level);
-    and and2(s2, air_humidity, low_water_level);
+    and and2(case2, air_humidity, low_water_level);
 
-    or or1(dripper_valvule, environment_conditions, s2);
+    or or1(dripper_valvule, environment_conditions, case2);
 
 endmodule 
