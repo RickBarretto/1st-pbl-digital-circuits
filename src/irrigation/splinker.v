f2e979a24273f8563743c5bae55a485c5b58f315
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-04-20 23:02:03 -0300

    [splinker] follow generic naming standard

--- src/irrigation/splinker.v
+++ src/irrigation/splinker.v
@@ -18,9 +18,9 @@ module splinker(
 
     wire s1, s2, s3, s4, s5;
 
-    not notS(s1, earth_humidity);
-    not notA(s2, air_humidity);
-    not notT(s3, low_temperature);
+    not not1(s1, earth_humidity);
+    not not2(s2, air_humidity);
+    not not3(s3, low_temperature);
     and and1(s4, s1, s2);
     and and2(s5, s1, s3, mid_water_level);
     or or1(splinker_bomb, s4, s5);
