ff2e2070e07a0886c07de7d3062cd10688ec2442
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-05-12 10:49:59 -0300

    [display] update documentation reducing noise and being more concise

--- src/display/display_decoder.v
+++ src/display/display_decoder.v
@@ -1,3 +1,8 @@
+//! Decodes binary code to output display
+//!
+//! See: 
+//!     ``display.md`` for more information.
+//!
 module display_decoder(
     output a, output b, output c,
     output d, output e, output f,
@@ -9,75 +14,72 @@ module display_decoder(
 	 
 );
 
-    // -----------------------------------------------------
-    // Segment A
+    //! Caption for formulas:
+    //!
+    //! seletor - A 
+	//! bit1 - B
+	//! bit0 - C
 
-	// seletor - A 
-	// bit1 - B
-	// bit0 - C
-    not (not1, selector);   // A'
-    not (not2, entry_Bit1); // B'
-	not (not3, entry_Bit0); // C'
-	 
-	and (and1, selector, entry_Bit1, entry_Bit0); // ABC
-	and (and2, not2, not3);     // B'C'
-	and (and3, not1, not2);     // A'B'
-	 
-    // ABC + B'C' + A'B'
+    //! Segment A
+    //! ---------
+    //! Formula: ABC + B'C' + A'B'
+
+    not (not1, selector);
+    not (not2, entry_Bit1); 
+	not (not3, entry_Bit0);
+
+	and (and1, selector, entry_Bit1, entry_Bit0);
+	and (and2, not2, not3);
+	and (and3, not1, not2);
+    
 	or (a, and1, and2, and3);
 
 
-    // -----------------------------------------------------
-    // Segment B
+    //! Segment B
+    //! ---------
+    //! Formula: B'C' + AB
 
-	and (and4, selector, entry_Bit1);   // AB
-
-    // B'C' + AB
+	and (and4, selector, entry_Bit1);
     or  (b, and4, and2); 
 
 
-    // -----------------------------------------------------
-    // Segment C
+    //! Segment C
+    //! ---------
+    //! Formula: ABC + A'C' + B'C'
 	 
 	and (and8, not1, not3);
-
-    // ABC + A'C' + B'C'
     or  (c, and1, and8, and2);  
     
 
-    // -----------------------------------------------------
-    // Segment D
+    //! Segment D
+    //! ---------
+    //! Formula: AC + B'
 	 
-	and (and5, selector, entry_Bit0);  // AC
-	 
-	// AC + B'
+	and (and5, selector, entry_Bit0); 
     or  (d, and5, not2); 
 
 
-    // -----------------------------------------------------
-    // Segment E
+    //! Segment D
+    //! ---------
+    //! Formula: B'C' + A'C + BC
 
-	and (and6, entry_Bit1, entry_Bit0);  // BC
+	and (and6, entry_Bit1, entry_Bit0);
 	and (and7, not1, entry_Bit0);
 
-    // B'C' + A'C + BC
     or  (   
         e, and2, and7, and6
     );
 
 
-    // -----------------------------------------------------
-    // Segment F
-
-
-    // B'C' + BC + A'
+    //! Segment F
+    //! ---------
+    //! Formula: B'C' + BC + A'
     or  (f, and2, and6, not1);
 
 
-    // -----------------------------------------------------
-    // Segment G
-
-    // B'C' + A'B' + AB
+    //! Segment G
+    //! ---------
+    //! Formula: B'C' + A'B' + AB
     or  (g, and2, and3, and4);
 
 
