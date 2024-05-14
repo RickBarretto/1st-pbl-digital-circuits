a03c46fc79140fddf30a1a559e449634efe2c396
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-05-13 22:36:54 -0300

    [project/utils] add documentation to mux

--- src/utils/multiplexer_2x1.v
+++ src/utils/multiplexer_2x1.v
@@ -1,3 +1,14 @@
+//! A simple 2x1 Multiplexer
+//!
+//! This may be used by other modules to select which data output, 
+//! according to FLOYD (2007).
+//!
+//! Reference
+//! ---------
+//! FLOYD, T. Funções da Lógica Combinacional. 
+//! In: JÚNIOR, A. P. (Ed.). Sistemas Digitais: Fundamentos e Aplicações. 
+//! Tradução: José Lucimar Do Nascimento. Porto Alegre: Bookman, 2007. p. 312–364. 
+//!
 module multiplexer_2x1(
     output out, 
     
